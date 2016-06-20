31f251171997d71d17990756a628d720a4df4cdc
patilshilpa <patilshilpa.mail@gmail.com>
2016-06-20 15:46:27 +0530

    first commit

---
+++ pack/packages/feedback/package.js
@@ -0,0 +1,30 @@
+Package.describe({
+  name: 'shilpa:feedback',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3.3.1');
+  api.use('ecmascript');
+   api.use("templating","client");
+   api.add_files("aTemplate.html","client");
+   /*api.add_files("main.css","client");*/
+  api.mainModule('feedback.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('shilpa:feedback');
+  api.mainModule('feedback-tests.js');
+});
+
+
+ 
